891e55f0d27cc819431d713e499c8a1bbccc752c
ryalinikhil <sameernikhil123@gmail.com>
2025-06-07 19:30:44 +0530

    Initial Commit

---
+++ Netflix./Netflix_App.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_App.swift
+//  Netflix.
+//
+//  Created by Sameer Nikhil on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
